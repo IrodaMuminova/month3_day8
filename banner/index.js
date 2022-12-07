47a642ce4248536acb29c233f6354904031120c6
IrodaMuminova <irodamuminova899@gmail.com>
2022-12-07 10:31:36 +0500

    initial

--- banner/index.js
+++ banner/index.js
@@ -2,8 +2,11 @@ let banner=document.querySelector(".banner");
 function f(){
     console.log("Banner bosildi")
 }
+let i=0;
 banner.addEventListener("click",(e)=>{
+    i++;
     e.preventDefault()
+    console.log(`${i}`);
     f();
 })
 
